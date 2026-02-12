b1f25f544b76b86838a6425d71b374df1539ad75
Tharun Siddhartha <otturutharun@gmail.com>
2026-02-12 19:14:30 +0530

    Created the submission for Player Score Calculator

---
+++ game_score.py
@@ -0,0 +1,18 @@
+
+
+
+player_name = str(input("Enter your Name: "))
+
+number_of_games_played = int(input("Enter the number of games played: "))
+
+
+total_score = int(input("Enter the total score: "))
+
+avg_score = total_score / number_of_games_played
+
+
+print(f"Player: {player_name}")
+print(f"Games Played: {number_of_games_played}")
+print(f"Total Score: {total_score}")
+print(f"Average Score: {avg_score}")
+
